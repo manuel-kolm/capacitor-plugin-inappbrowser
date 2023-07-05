9a84e0c3436e50f3d031d964f95ea9a14c52437b
manuel-kolm <83550123+manuel-kolm@users.noreply.github.com>
2023-07-05 22:09:01 +0200

    Add Link handling for Android

--- android/src/main/java/com/capacitor/plugin/inappbrowser/InAppBrowser.java
+++ android/src/main/java/com/capacitor/plugin/inappbrowser/InAppBrowser.java
@@ -1,11 +1,15 @@
 package com.capacitor.plugin.inappbrowser;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,6 +31,34 @@ public class InAppBrowser {
             webView = new WebView(context);
             webView.loadUrl(uri.toString());
             activity.addContentView(webView, new WindowManager.LayoutParams(WindowManager.LayoutParams.FILL_PARENT, WindowManager.LayoutParams.FILL_PARENT));
+
+            webView.getSettings().setSupportMultipleWindows(true);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.setWebChromeClient(new WebChromeClient() {
+                @Override
+                public boolean onCreateWindow(WebView view, boolean dialog, boolean userGesture, android.os.Message resultMsg)
+                {
+                    String data =  view.getHitTestResult().getExtra();
+                    Context context = view.getContext();
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(data));
+                    context.startActivity(browserIntent);
+                    return false;
+                }
+            });
+            webView.setWebViewClient(new WebViewClient(){
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                    String url = request.getUrl().toString();
+                    if (url != null && (url.startsWith("tel://") || url.startsWith("mail://"))) {
+                        view.getContext().startActivity(
+                                new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            });
         });
     }
 
